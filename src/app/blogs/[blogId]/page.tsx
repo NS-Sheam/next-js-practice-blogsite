ff2b8bcf10eb14b0ecd8871631a5c843a73d1b42
NS-Sheam <123sheamfeni@gmail.com>
2024-04-23 22:03:37 +0600

    add generateStaticParams

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,5 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
 import React from "react";
 
 interface BlogId {
@@ -6,6 +7,14 @@ interface BlogId {
     blogId: string;
   };
 }
+
+const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
 
 const BlogDetailsPage = async ({ params }: BlogId) => {
   const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
