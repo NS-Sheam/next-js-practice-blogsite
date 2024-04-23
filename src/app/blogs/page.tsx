5e78011a9c4761aec81d1e733a94ba67af91eb56
NS-Sheam <123sheamfeni@gmail.com>
2024-04-23 21:38:11 +0600

    first commit

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BlogsPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center my-5">
+        All blogs From <span className="text-accent">Blogiz</span>
+      </h1>
+      <p className="text-xl text-center text-gray-400 w-2/4 mx-auto ">
+        <i>Dive into the fascinating world of quantum computing, where unlocking unprecedented computational power.</i>
+      </p>
+    </div>
+  );
+};
+
+export default BlogsPage;
