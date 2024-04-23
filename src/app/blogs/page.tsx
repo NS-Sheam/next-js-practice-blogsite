672f0f0f8c2dc4f3251b4238bce720365c373e34
NS-Sheam <123sheamfeni@gmail.com>
2024-04-23 22:27:30 +0600

    Refactor BlogsPage component to use Redux

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,12 +1,18 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/types";
 import React from "react";
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store",
-  });
-  const blogs = await res.json();
+// const BlogsPage = async () => {
+const BlogsPage = () => {
+  //   const res = await fetch("http://localhost:5000/blogs", {
+  //     cache: "no-store",
+  //   });
+  //   const blogs = await res.json();
+
+  const { data: blogs, isLoading, isError, error } = useGetBlogsQuery("");
+
   return (
     <div className="w-[90%] mx-auto">
       <h1 className="text-4xl text-center my-5">
@@ -16,7 +22,7 @@ const BlogsPage = async () => {
         <i>Dive into the fascinating world of quantum computing, where unlocking unprecedented computational power.</i>
       </p>
       <div className="grid grid-cols-3 gap-4 my-5">
-        {blogs.map((blog: Blog) => (
+        {blogs?.map((blog: Blog) => (
           <BlogCard
             key={blog.id}
             blog={blog}
